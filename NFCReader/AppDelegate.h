10cb2413c25d7651a8c7b681633903eb588181c2
Bosko Petreski <bosko@boskos-macbook-pro.local>
2018-04-03 22:00:10 +0200

    Initial Commit

---
+++ NFCReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NFCReader
+//
+//  Created by Bosko Petreski on 4/3/18.
+//  Copyright © 2018 Bosko Petreski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NFCReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NFCReader
+//
+//  Created by Bosko Petreski on 4/3/18.
+//  Copyright © 2018 Bosko Petreski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
